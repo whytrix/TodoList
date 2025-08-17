4d5131e8bb84f0c6cd8d39cf496ebc41fc93eb3c
whytrix <1878631986@qq.com>
2025-08-17 16:23:03 +0800

    hard code access data and render views

--- TodoList/Controllers/HomeController.cs
+++ TodoList/Controllers/HomeController.cs
@@ -1,16 +1,57 @@
 using Microsoft.AspNetCore.Mvc;
 using TodoList.Models;
 using TodoList.ViewModels;
+using TodoList.Enums;
 
 namespace TodoList.Controllers
 {
     public class HomeController : Controller
     {
+
+        private static List<TodoItem> todoItems = new List<TodoItem>
+            {
+                new TodoItem
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "学习ASP.NET Core",
+                    Description = "学习ASP.NET Core的基础知识和高级特性",
+                    DueDate = DateTime.Now.AddDays(7),
+                    Priority = PriorityLevel.High,
+                    IsCompleted = false
+                },
+                new TodoItem
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "完成项目报告",
+                    Description = "撰写并提交项目报告",
+                    DueDate = DateTime.Now.AddDays(3),
+                    Priority = PriorityLevel.Medium,
+                    IsCompleted = true
+                }
+            };
+
         [HttpGet("")]
         [HttpGet("Index")]
         public IActionResult Index()
         {
-            return View();
+            return View(todoItems);
+        }
+
+        [HttpGet("Details/{id}")]
+        public IActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound("ID不能为空");
+            }
+
+            if (!Guid.TryParse(id, out Guid todoId))
+            {
+                return BadRequest("无效的ID格式");
+            }
+
+            var todoItem = todoItems.FirstOrDefault(t => t.Id == todoId);
+            return View(todoItem);
         }
 
         [HttpPost("Create")]
